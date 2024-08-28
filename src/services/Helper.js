99a7bf155c4f7dc36de7f65afdaf71c7afc124bd
Mohit Singh <msakra000@gmail.com>
2024-08-29 02:55:22 +0530

    updated helper.js

--- src/services/Helper.js
+++ src/services/Helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from '../auth';
 
-export const Base_URL = 'https://blogapp-3mfe.onrender.com/api/v1';
+export const Base_URL = 'https://server-blogapp.up.railway.app/api/v1';
 
 export const myAxios = axios.create({
     baseURL: Base_URL
